216aa0314d91b44a67beade05a003e86cdaeac26
Tish <latisha.griffiths@programmer.net>
2019-04-03 15:33:44 -0500

    Added navbar, added mongodb cloud, mongoose, and dotenv, campgrounds now persist

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-const bodyParser = require("body-parser");
+const express    = require("express"),
+      app        = express(),
+      port       = process.env.PORT || 3000,
+      bodyParser = require("body-parser"),
+      path       = require("path"),
+      dotenv     = require('dotenv').config(),
+      URI        = `mongodb+srv://kelpCampCreator:${process.env.kelpCampCreatorPassword}@kelpcampapp-rwgto.mongodb.net/test?retryWrites=true`,
+      mongoose   = require("mongoose");
+      mongoose.connect(URI, { useNewUrlParser: true }, (err) => {
+          if(err) {
+              console.log(err.errors);
+          } else {
+              console.log("successully connected to database!")
+          }
+      })
+      //Schema setup
+      const campgroundSchema = new mongoose.Schema({
+          name: String,
+          image: String
+      })
+
+      const Campground = mongoose.model("Campground", campgroundSchema);
+
+    //   Campground.create({name: "Blooper Mountain", image: "https://images.unsplash.com/photo-1534067783941-51c9c23ecefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"}, (err, campground) => {
+    //       if(err) console.log(err);
+    //       else console.log('successfully created new campground: ', campground);
+    //   })
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
-let campgrounds = [
-    {name: "Sunny Hill", image: "https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-    {name: "Blooper Mountain", image: "https://images.unsplash.com/photo-1534067783941-51c9c23ecefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-    {name: "IDK Jeesh", image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"}
-];
+app.use(express.static(path.join(__dirname + "/assets")));
 
 app.get("/", (req, res, next) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res, next) => {
-    res.render("Campgrounds", {campgrounds});
+    Campground.find({}, (err, allCampgrounds) => {
+        if(err) console.log(err);
+        else res.render("campgrounds", {campgrounds: allCampgrounds});
+    })
 })
 
 app.post("/campgrounds", (req, res, next) => {
@@ -25,9 +46,13 @@ app.post("/campgrounds", (req, res, next) => {
     let name = req.body.name;
     let image = req.body.image;
     let newCampground = {name, image};
-    campgrounds.push(newCampground);
+    //create a newCampground and save to db
+    Campground.create(newCampground, (err, newCampgroundCreated) => {
+        if(err) console.log(err);
+        else res.redirect("/campgrounds");
+        console.log('Successfully created a new campground... ', newCampgroundCreated);
+    })
     //redirect back to campgrounds page
-    res.redirect("/campgrounds");
 })
 
 app.get("/campgrounds/new/", (req, res, next) => {
